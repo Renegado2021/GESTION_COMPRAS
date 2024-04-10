e5f25a74ea8e8b14a9ff0a647b43a7aab3c55677
Renegado2021 <dennis.chavez@unah.hn>
2024-04-09 18:24:51 -0600

    recuperacion por contraseña

--- Proveedores/actualizar_proveedores.php
+++ Proveedores/actualizar_proveedores.php
@@ -2,7 +2,7 @@
 // Configuración de la conexión a la base de datos
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 $dbname = "gestion_compras2";
 
 // Crear la conexión
